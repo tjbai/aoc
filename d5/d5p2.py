ed5ae9be9675e64c4c2af36560a43fc74a319b42
TJ Bai <tjianbai@gmail.com>
2023-12-05 12:53:10 -0500

    d5p2

--- d5/d5p2.py
+++ d5/d5p2.py
@@ -28,20 +28,43 @@ for ch in chunks[1:]:
 ranges = {k: sorted(v, key=lambda x: x[1]) for k, v in ranges.items()}
 range_ends = {k: sorted(v) for k, v in range_ends.items()}
 
-def get_mapping(source: str, n: int) -> tuple:
+def get_mapping(source: str, l: int, r: int) -> tuple:
+    cur_ranges = ranges[source]
     dest = mapping[source]
-    i = bisect_left(range_ends[source], n)
-    if i >= len(ranges[source]): return (dest, n)
-    if n < ranges[source][i][0]: return (dest, n)
-    return (dest, n + ranges[source][i][2])
+    dest_ranges = []
     
-def traverse(source: str, n: int) -> int:
-    while source != 'location': source, n = get_mapping(source, n)
-    return n
+    i = bisect_left(range_ends[source], l)
+    if i >= len(cur_ranges) or r < cur_ranges[i][0]:
+        return (dest, [(l, r)])
+    
+    while i < len(cur_ranges) and r >= cur_ranges[i][0] and l < r:
+        range_l, range_r, diff = cur_ranges[i]
+        if l < range_l:
+            dest_ranges.append((l, range_l-1))
+            l = range_l
+        dest_ranges.append((l+diff, min(r, range_r)+diff))
+        l = min(r, range_r) + 1
+        i += 1
+    
+    if l < r: dest_ranges.append((l, r))
+    
+    return dest, dest_ranges
+    
+def traverse(source: str, ranges: list) -> int:
+    while source != 'location':
+        new_ranges = []
+        for r in ranges:
+            print(source, 'processing', r)
+            dest, dest_ranges = get_mapping(source, r[0], r[1])
+            new_ranges.extend(dest_ranges)
+        source = dest
+        ranges = new_ranges[:]
+
+    return min(r[0] for r in ranges)
 
 res = float('inf')
 for i in range(0, len(seeds), 2):
     l, r = seeds[i], seeds[i+1]
-    for i in range(r): res = min(res, traverse('seed', l+i))
+    res = min(res, traverse('seed', [(l, l+r-1)]))
    
 print(res)
