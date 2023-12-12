b190d5d6b68c35868f9119d2d26141f7256844dc
TJ Bai <tjianbai@gmail.com>
2023-12-11 19:53:55 -0500

    d10 p2 progress

--- d10/d10p2.py
+++ d10/d10p2.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 
 from sys import argv
+from collections import defaultdict, deque
 
 if len(argv) > 1: input_file = 'd.in'
 else: input_file = 'sample.in'
@@ -8,39 +9,54 @@ else: input_file = 'sample.in'
 with open(input_file) as f: s = f.read() 
 
 dirs = {
-    '|': [(0,-1),(0,1)],
-    '-': [(-1,0),(1,0)],
-    'L': [(0,-1),(1,0)],
-    'J': [(0,1),(1,0)],
-    '7': [(-1,0),(0,1)],
-    'F': [(-1,0),(0,-1)],
+    '|': [(-1,0),(1,0)],
+    '-': [(0,-1),(0,1)],
+    'L': [(-1,0),(0,1)],
+    'J': [(0,-1),(-1,0)],
+    '7': [(0,-1),(1,0)],
+    'F': [(0,1),(1,0)],
     '.': [],
     'S': []
 }
 
-grid = [[c for c in line] for line in s.split('\n')]
-M, N = len(grid), len(grid[0])
-seen = set()
+g = [[c for c in line] for line in s.split('\n')]
+M, N = len(g), len(g[0])
 
-def dfs(r, c, pr, pc) -> bool:
-    if r < 0 or c < 0 or r >= M or c >= N: return False
-    if (r, c) in seen: return True
-    seen.add((r, c))
-    
-    if grid[r][c] not in {'.', 'O', 'I'}:
-        is_border = False
-        for dr, dc in dirs[grid[r][c]]: is_border |= r+dr == pr and c+dc == c
-        return is_border
-
-    aa, ab, ac, ad = dfs(r+1, c, r, c) , dfs(r-1, c, r, c) , dfs(r, c+1, r, c) , dfs(r, c-1, r, c)
-    print(r, c, aa, ab, ac, ad)
-    return True
-    # grid[r][c] = 'I' if valid else 'O'
-    # return valid
-
+into = defaultdict(set)
+outo = defaultdict(set)
 for r in range(M):
     for c in range(N):
-        if grid[r][c] == '.': dfs(r, c, r, c)
+        if g[r][c] == 'S': sr, sc = r, c
+        for dr, dc in dirs[g[r][c]]:
+            into[(r,c)].add((r+dr,c+dc))
+            outo[(r,c)].add((r+dr,c+dc))
 
-for row in grid:
-    print(' '.join(row))
+outo[(sr,sc)] = {(sr+1,sc),(sr-1,sc),(sr,sc+1),(sr,sc-1)}
+q = deque([(sr,sc,0)])
+pipe = set()
+
+while q:
+    cr, cc, d = q.popleft()
+    if (cr, cc) in pipe: continue
+    pipe.add((cr, cc))
+    for nr,nc in outo[(cr, cc)]:
+        if (cr, cc) in into[(nr, nc)]:
+            q.append((nr, nc, d+1))
+            
+for r in range(M):
+    for c in range(N):
+        if (r, c) not in pipe: g[r][c] = '.'
+
+def raycast(r: int) -> int:
+    res = 0
+    inside = False
+    for c in range(N):
+        if g[r][c] in {'|', 'L', '7', 'S'}: inside = not inside
+        if inside and g[r][c] == '.': res += 1
+    return res
+
+for r in range(M):
+    print(' '.join(g[r]))
+    print(raycast(r))
+    # print()
+# print(sum(raycast(r) for r in range(M)))
