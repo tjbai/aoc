19146375d836d62306786362e03c46fbb7db64ce
TJ Bai <tjianbai@gmail.com>
2023-12-21 02:36:58 -0500

    d17

---
+++ d17/d17p2.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python3
+
+from sys import argv
+from collections import defaultdict
+from heapq import heappush, heappop
+
+if len(argv) > 1: input_file = 'd.in'
+else: input_file = 'sample.in'
+
+with open(input_file) as f: s = f.read() 
+
+g = [[int(c) for c in line] for line in s.split()]
+M, N = len(g), len(g[0])
+
+nodes = [(0, 0, 0, 1, 0, 1), (0, 0, 0, 0, 1, 1)]
+dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+min_d = defaultdict(lambda: float('inf'))
+seen = set()
+
+while nodes:
+    d, r, c, dr, dc, n = heappop(nodes)
+    
+    if n > 10: continue
+    if (r, c, dr, dc, n) in seen: continue
+    seen.add((r, c, dr, dc, n))
+    min_d[(r, c)] = min(min_d[(r, c)], d)
+    
+    for ddr, ddc, in dirs:
+        nr, nc = r+ddr, c+ddc
+        if nr < 0 or nc < 0 or nr == M or nc == N: continue
+        if (ddr, ddc) == (-dr, -dc): continue
+        
+        if (ddr, ddc) == (dr, dc): heappush(nodes, (d+g[nr][nc], r+ddr, c+ddc, ddr, ddc, n+1))
+        elif n >= 4: heappush(nodes, (d+g[nr][nc], r+ddr, c+ddc, ddr, ddc, 1))
+        
+print(min_d[(M-1, N-1)])
