4f0ce3430eb58df89f7bc31d72151cba6f45028b
TJ Bai <tjianbai@gmail.com>
2023-12-24 23:31:00 -0500

    p2 work, kinda lazy

--- d24/d24p2.py
+++ d24/d24p2.py
@@ -17,3 +17,14 @@ if len(sys.argv) > 1: input_file = 'd.in'
 else: input_file = 'sample.in'
 
 with open(input_file) as f: s = f.read() 
+
+'''
+now considering all coordinates find a starting position, velocity pair
+that will allow us to hit ALL hailstones at some point in the future
+
+idea (with hint):
+p(t) = v * t + p_0
+
+for each (v_i, p_0i) we need v * t + p_0 = v_i * t + p_0i
+=> (v - v_i) * t + (p_0 - p_0i) = 0
+'''
