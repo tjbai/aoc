66e5879b5af72e9c8188145154830c89ef8c1efa
TJ Bai <tjianbai@gmail.com>
2024-12-01 01:51:07 -0500

    removed 3 tokens

--- 2024/d1/d1p2.py
+++ 2024/d1/d1p2.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 with open('d.in', 'r') as f:
-    ls1, ls2 = zip(*[map(int, line.split()) for line in f])
+    ls1, ls2 = zip(*[line.split() for line in f])
     ct = Counter(ls2)
 
-print(sum(x*ct[x] for x in ls1))
+print(sum(int(x)*ct[x] for x in ls1))
