892c7fecfda16808e58d8988a183aa8d65424d21
TJ Bai <tjianbai@gmail.com>
2024-12-01 02:03:03 -0500

    removed encapsulating objects

--- 2024/d1/d1p1.scala
+++ 2024/d1/d1p1.scala
@@ -1,11 +1,10 @@
-object D1P1:
-  @main def p1(args: String*): Unit =
-    var input = scala.io.Source
-      .fromFile(if args.length > 0 then "d.in" else "sample.in")
-      .getLines
-      .toList
+@main def p1(args: String*): Unit =
+  var input = scala.io.Source
+    .fromFile(if args.length > 0 then "d.in" else "sample.in")
+    .getLines
+    .toList
 
-    val ls1 = input.map(_.split("\\s+")(0).toInt).sorted
-    val ls2 = input.map(_.split("\\s+")(1).toInt).sorted
+  val ls1 = input.map(_.split("\\s+")(0).toInt).sorted
+  val ls2 = input.map(_.split("\\s+")(1).toInt).sorted
 
-    println(ls1.zip(ls2).map((a, b) => (a - b).abs).sum)
+  println(ls1.zip(ls2).map((a, b) => (a - b).abs).sum)
--- 2024/d1/d1p2.scala
+++ 2024/d1/d1p2.scala
@@ -1,14 +1,13 @@
-object D1P2:
-  @main def p2(args: String*): Unit =
-    val ls = scala.io.Source
-      .fromFile(if args.length > 0 then "d.in" else "sample.in")
-      .getLines
-      .map(_.split("\\s+").map(_.toInt))
-      .toList
+@main def p2(args: String*): Unit =
+  val ls = scala.io.Source
+    .fromFile(if args.length > 0 then "d.in" else "sample.in")
+    .getLines
+    .map(_.split("\\s+").map(_.toInt))
+    .toList
 
-    val freq = ls
-      .map(_(1))
-      .groupBy(identity)
-      .mapValues(_.length)
+  val freq = ls
+    .map(_(1))
+    .groupBy(identity)
+    .mapValues(_.length)
 
-    println(ls.map(x => x(0) * freq.getOrElse(x(0), 0)).sum)
+  println(ls.map(x => x(0) * freq.getOrElse(x(0), 0)).sum)
