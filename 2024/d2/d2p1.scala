b8015a66d3728a4817e4032f5ef823b2f608d9f6
TJ Bai <tjianbai@gmail.com>
2024-12-03 14:21:16 -0500

    d2p2 fin

--- 2024/d2/d2p1.scala
+++ 2024/d2/d2p1.scala
@@ -1,8 +1,9 @@
-def isSafe(ls: Array[Int]): Boolean =
-  val diffs = ls.zip(ls.tail).map(_ - _)
-  diffs.forall(x => 1 <= x && x <= 3) || diffs.forall(x => -1 >= x && x >= -3)
-
 @main def d2p1(args: String*): Unit =
+
+  def isSafe(ls: Array[Int]): Boolean =
+    val diffs = ls.zip(ls.tail).map(_ - _)
+    diffs.forall(x => 1 <= x && x <= 3) || diffs.forall(x => -1 >= x && x >= -3)
+
   println(
     scala.io.Source
       .fromFile(if args.length > 0 then "d.in" else "sample.in")
--- 2024/d2/d2p2.scala
+++ 2024/d2/d2p2.scala
@@ -1,7 +1,25 @@
-@main def main(full: Boolean = false): Unit =
- val input = full match {
-   case false => scala.io.Source.fromFile("sample.in").getLines
-   case true  => scala.io.Source.fromFile("d.in").getLines
- }
+@main def d2p2(args: String*): Unit =
 
- println("")
+  val inc = (x: Int, y: Int) => 1 <= (x - y) && (x - y) <= 3
+  val dec = (x: Int, y: Int) => -1 >= (x - y) && (x - y) >= -3
+
+  def isSafe(ls: List[Int], cond: (Int, Int) => Boolean): Boolean =
+    def aux(ls: List[Int], hist: (Int, Int), life: Boolean): Boolean =
+      (ls, hist) match
+        case (hd :: tl, (pp, p)) if cond(p, hd) => aux(tl, (p, hd), life)
+        case (hd :: tl, (pp, p)) if !cond(p, hd) && !life => false
+        case (hd :: tl, (pp, p)) if !cond(p, hd) =>
+          aux(tl, (pp, p), false) || aux(ls, (pp, pp), false)
+        case _ => true
+
+    ls match
+      case a :: b :: c => aux(b :: c, (a, a), true) || aux(c, (b, b), false)
+      case _           => true // short lists will always be satisfiable
+
+  println(
+    scala.io.Source
+      .fromFile(if args.length > 0 then "d.in" else "sample.in")
+      .getLines
+      .map(_.split("\\s+").map(_.toInt).toList)
+      .count(ls => isSafe(ls, inc) || isSafe(ls, dec))
+  )
