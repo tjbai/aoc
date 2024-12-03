4b06f5d2c3e86448de93f7ffd1141b33af213518
TJ Bai <tjianbai@gmail.com>
2024-12-03 15:19:17 -0500

    cleaned up a bit

--- 2024/d3/d3p1.scala
+++ 2024/d3/d3p1.scala
@@ -1,39 +1,36 @@
 enum Token:
-  case Mul
-  case LParen
-  case RParen
-  case Sep
-  case Empty
+  case Mul, Lparen, Rparen, Sep, Empty
   case Num(value: Int)
-import Token.*
 
-def tokenize(
-    ls: Seq[Char],
-    tokens: List[Token] = List(),
-    num: List[Char] = List()
-): List[Token] =
-  (ls, num) match
-    case (d :: tl, _) if d.isDigit => tokenize(tl, tokens, d :: num)
-    case (d :: tl, hd :: _) if !d.isDigit =>
-      val numTok = Num(num.reverse.mkString("").toInt)
-      tokenize(d :: tl, numTok :: tokens, List())
-    case ('(' :: tl, _)               => tokenize(tl, LParen :: tokens, num)
-    case (')' :: tl, _)               => tokenize(tl, RParen :: tokens, num)
-    case ('m' :: 'u' :: 'l' :: tl, _) => tokenize(tl, Mul :: tokens, num)
-    case (',' :: tl, _)               => tokenize(tl, Sep :: tokens, num)
-    case (_ :: tl, _)                 => tokenize(tl, Empty :: tokens, num)
-    case (_, _)                       => tokens.reverse
+extension (source: Seq[Char])
+  def tokenize: List[Token] =
+    import Token.*
+    def aux(ls: Seq[Char], tokens: List[Token], num: List[Char]): List[Token] =
+      (ls, num) match
+        case (d :: tl, _) if d.isDigit => aux(tl, tokens, d :: num)
+        case (d :: tl, hd :: _) => aux(d :: tl, Num(num.reverse.mkString.toInt) :: tokens, List())
+        case ('(' :: tl, _)     => aux(tl, Lparen :: tokens, num)
+        case (')' :: tl, _)     => aux(tl, Rparen :: tokens, num)
+        case ('m' :: 'u' :: 'l' :: tl, _) => aux(tl, Mul :: tokens, num)
+        case (',' :: tl, _)               => aux(tl, Sep :: tokens, num)
+        case (_ :: tl, _)                 => aux(tl, Empty :: tokens, num)
+        case (_, _)                       => tokens.reverse
+    aux(source, List(), List())
 
-def eval(tokens: List[Token], acc: Int = 0): Int =
-  tokens match
-    case Mul :: LParen :: Num(a) :: Sep :: Num(b) :: RParen :: tl => eval(tl, acc + a * b)
-    case _ :: tl                                                  => eval(tl, acc)
-    case _                                                        => acc
+extension (tokens: List[Token])
+  def eval: Int =
+    import Token.*
+    def aux(tokens: List[Token], acc: Int): Int = tokens match
+      case Mul :: Lparen :: Num(a) :: Sep :: Num(b) :: Rparen :: tl => aux(tl, acc + a * b)
+      case _ :: tl                                                  => aux(tl, acc)
+      case _                                                        => acc
+    aux(tokens, 0)
 
 @main def d3p1(args: String*): Unit =
-  val source = scala.io.Source
-    .fromFile(if args.length > 0 then "d.in" else "sample.in")
-    .toSeq
-
-  // no pipelines hurts
-  println(eval(tokenize(source)))
+  println(
+    scala.io.Source
+      .fromFile(if args.length > 0 then "d.in" else "sample.in")
+      .toSeq
+      .tokenize
+      .eval
+  )
