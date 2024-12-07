0869d109cdf4ea7ec546394fa49f6d6f1e97909e
TJ Bai <tjianbai@gmail.com>
2024-12-07 14:01:01 -0500

    d7 fin

---
+++ 2024/d7/d7p1.scala
@@ -0,0 +1,17 @@
+@main def d7p1(args: String*): Unit =
+
+  def solve(targ: Long, arr: Array[Long], idx: Int = 0, acc: Long = 0): Boolean =
+    if acc > targ then false
+    else if idx == arr.length then acc == targ
+    else solve(targ, arr, idx + 1, acc + arr(idx)) || solve(targ, arr, idx + 1, acc * arr(idx))
+
+  println(
+    scala.io.Source
+      .fromFile(if args.length > 0 then "d.in" else "sample.in")
+      .getLines
+      .map(_.split(":"))
+      .map(x => (x(0).toLong, x(1).split(" ").drop(1).map(_.toLong)))
+      .filter(x => solve(x(0), x(1)))
+      .map(_(0))
+      .sum
+  )
