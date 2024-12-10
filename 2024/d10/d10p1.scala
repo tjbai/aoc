1eff38bf70a4a098f31b604b181cb609d2b14156
TJ Bai <tjianbai@gmail.com>
2024-12-10 00:37:04 -0500

    d10 fin

---
+++ 2024/d10/d10p1.scala
@@ -0,0 +1,30 @@
+import scala.collection.mutable.Set
+
+@main def d10p1(args: String*): Unit =
+  val lines = scala.io.Source
+    .fromFile(if args.length > 0 then "d.in" else "sample.in")
+    .getLines
+    .map(_.map(_ - '0'))
+    .toIndexedSeq
+
+  val (rows, cols) = (lines.length, lines(0).length)
+
+  def search(r: Int, c: Int, prev: Int, seen: Set[(Int, Int)]): Int =
+    if seen((r, c))
+      || r < 0 || c < 0 || r >= rows || c >= cols
+      || lines(r)(c) != prev + 1
+    then 0
+    else
+      seen += ((r, c))
+      if lines(r)(c) == 9 then 1
+      else
+        search(r + 1, c, prev + 1, seen)
+          + search(r, c + 1, prev + 1, seen)
+          + search(r - 1, c, prev + 1, seen)
+          + search(r, c - 1, prev + 1, seen)
+
+  println((for
+    r <- 0 until rows
+    c <- 0 until cols
+    if lines(r)(c) == 0
+  yield search(r, c, -1, Set())).sum)
