0f5b674fe1deadfb7f6daaa00dc302ac41af65e3
TJ Bai <tjianbai@gmail.com>
2024-12-11 01:15:14 -0500

    d11p2 fin

--- 2024/d11/d11p2.scala
+++ 2024/d11/d11p2.scala
@@ -1,4 +1,28 @@
+import scala.collection.mutable.Map
+
 @main def d11p2(args: String*): Unit =
-  val lines = scala.io.Source
+  val line = scala.io.Source
     .fromFile(if args.length > 0 then "d.in" else "sample.in")
     .getLines
+    .next
+    .split(" ")
+    .map(_.toLong)
+
+  var cache = Map[(Long, Long), Long]()
+  def expand(stone: Long, depth: Int): Long =
+    cache.getOrElseUpdate(
+      (stone, depth), {
+        if depth == 0 then 1
+        else
+          stone.toString match
+            case "0" =>
+              expand(1L, depth - 1)
+            case s if s.length % 2 == 0 =>
+              val (fh, bh) = s.splitAt(s.length / 2)
+              expand(fh.toLong, depth - 1) + expand(bh.toLong, depth - 1)
+            case _ =>
+              expand(stone * 2024, depth - 1)
+      }
+    )
+
+  println(line.map(expand(_, 75)).sum)
