625b6f0e7bf9fce1f0bf7bc4feb950ae07fff2f9
TJ Bai <tjianbai@gmail.com>
2024-12-11 00:27:15 -0500

    p1 fin

--- 2024/d11/d11p1.scala
+++ 2024/d11/d11p1.scala
@@ -1,4 +1,23 @@
 @main def d11p1(args: String*): Unit =
-  val lines = scala.io.Source
+  def update(stone: Long): Seq[Long] =
+    stone.toString match
+      case "0" => Seq(1L)
+      case s if s.length % 2 == 0 =>
+        val (fh, bh) = s.splitAt(s.length / 2)
+        Seq(fh.toLong, bh.toLong)
+      case _ => Seq(stone * 2024)
+
+  val line = scala.io.Source
     .fromFile(if args.length > 0 then "d.in" else "sample.in")
     .getLines
+    .next
+    .split(" ")
+    .map(_.toLong)
+
+  val len = LazyList
+    .iterate(line)(_.flatMap(update))
+    .take(26)
+    .last
+    .length
+
+  println(len)
